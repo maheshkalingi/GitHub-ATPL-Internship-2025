4f8a35851f9eaba80b7312e99f11d8bca4bf99f1
mahesh <maheshkalingi99@gmail.com>
2025-07-19 09:52:25 +0530

    assignment 11 is added

--- Assignment11/assignment11/src/app/task1/task1.ts
+++ Assignment11/assignment11/src/app/task1/task1.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-task1',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task1.html',
   styleUrl: './task1.css'
 })
