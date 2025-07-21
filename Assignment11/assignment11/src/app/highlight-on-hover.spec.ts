b823f6e60cf8daea6e1a725d7ff22f6febf56009
mahesh <maheshkalingi99@gmail.com>
2025-07-21 08:08:43 +0530

    assignment 11 is added

--- Assignment11/assignment11/src/app/highlight-on-hover.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightOnHover } from './highlight-on-hover';
-
-describe('HighlightOnHover', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightOnHover();
-    expect(directive).toBeTruthy();
-  });
-});
