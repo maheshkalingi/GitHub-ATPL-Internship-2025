d5a96aa5e0496d9042ad1778888562580e5885fa
mahesh <maheshkalingi99@gmail.com>
2025-07-22 17:30:36 +0530

    assignment 13 is added

---
+++ assignment13/assignment13/src/app/task5/task5.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task5',
+  imports: [],
+  templateUrl: './task5.html',
+  styleUrl: './task5.css'
+})
+export class Task5 {
+
+}
