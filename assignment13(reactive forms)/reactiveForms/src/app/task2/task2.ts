cd8851104a556612d0c6c2e8984169ed4241a223
mahesh <maheshkalingi99@gmail.com>
2025-07-23 17:34:50 +0530

    assignment 13(2) is added

---
+++ assignment13(reactive forms)/reactiveForms/src/app/task2/task2.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task2',
+  imports: [],
+  templateUrl: './task2.html',
+  styleUrl: './task2.css'
+})
+export class Task2 {
+
+}
