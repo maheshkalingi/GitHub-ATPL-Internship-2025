7ad023a71b546d8bb3070fd516d098c459509274
muraliaditya <muraliaditya06@gmail.com>
2025-08-05 23:29:13 +0530

    assignment 19 is added

--- assignment19/rxjs/src/app/app.routes.ts
+++ assignment19/rxjs/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Task1Component } from './task1/task1';
 import { Task2 } from './task2/task2';
 import { Task3 } from './task3/task3';
-// import { Task4 } from './task4/task4';
+import { Task4 } from './task4/task4';
 import { Task5 } from './task5/task5';
 import { Task6 } from './task6/task6';
 
@@ -11,7 +11,7 @@ export const routes: Routes = [
   { path: 'task1', component: Task1Component },
   { path: 'task2', component: Task2 },
   { path: 'task3', component: Task3 },
-//   { path: 'task4', component: Task4 },
+  { path: 'task4', component: Task4 },
   { path: 'task5', component: Task5 },
   { path: 'task6', component: Task6 }
 ];
--- assignment19/rxjs/src/app/task4/task4.ts
+++ assignment19/rxjs/src/app/task4/task4.ts
@@ -1,18 +1,18 @@
-// import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { BehaviorSubject } from 'rxjs';
-// import { NzButtonModule } from 'ng-zorro-antd/button';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
-// @Component({
-//   selector: 'app-task4',
-//   imports: [CommonModule, NzButtonModule],
-//   templateUrl: './task4.html',
-//   styleUrl: './task4.css'
-// })
-// export class Task4 {
-//  theme$ = new BehaviorSubject<'light' | 'dark'>('light');
+@Component({
+  selector: 'app-task4',
+  imports: [CommonModule, NzButtonModule],
+  templateUrl: './task4.html',
+  styleUrl: './task4.css'
+})
+export class Task4 {
+ theme$ = new BehaviorSubject<'light' | 'dark'>('light');
 
-//   setTheme(theme: 'light' | 'dark') {
-//     this.theme$.next(theme);
-//   }
-// }
+  setTheme(theme: 'light' | 'dark') {
+    this.theme$.next(theme);
+  }
+}
