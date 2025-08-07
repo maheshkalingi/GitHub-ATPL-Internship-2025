9d64a0f3c9938f02ad55f1bf92e13c662059e958
mahesh <maheshkalingi99@gmail.com>
2025-08-07 17:40:25 +0530

    assignment 1 is started

--- Assignment-1/SecondLargestElement.java
+++ Assignment-1/SecondLargestElement.java
@@ -1,5 +1,16 @@
 public class SecondLargestElement {
     public static void main(String[] args){
+     int arr[]={10,5,20,8,15};
+     int b=arr.length;
+     for(int i=0;i<b;i++){
+        for(int j=i+1;j<=i;j++){
+            if(arr[i]>arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[i]=temp;
+            }
+        }
+     }
      
     }
 }
