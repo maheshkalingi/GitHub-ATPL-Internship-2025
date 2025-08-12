6b4bb21a69d4ba9de68fe21ba7e653f429a4c1cc
mahesh <maheshkalingi99@gmail.com>
2025-08-12 09:00:42 +0530

    Assignment 3 is added

--- Assignment-3/ConcateString.java
+++ Assignment-3/ConcateString.java
@@ -4,25 +4,37 @@ public class ConcateString {
         this.names=Names;
     }
    public void usingplusOperator(){
+    long startTime = System.nanoTime();
     String Combined="";
     for(int i=0;i<names.length;i++){
         Combined+=names[i];
     }
-     System.out.println("By Using + Operator : "+Combined);
+     long endTime = System.nanoTime();
+      long Time = endTime - startTime; 
+     System.out.println("By Using + Operator : "+Combined );
+     System.out.println("Executed time in nanoseconds: " + Time);
    }
    public void usingBuilder(){
+    long startTime = System.nanoTime();
   StringBuilder Combined=new StringBuilder("");
     for(int i=0;i<names.length;i++){
       Combined.append(names[i]);
     }
+     long endTime = System.nanoTime();
+      long Time = endTime - startTime; 
  System.out.println("By Using StringBuilder: "+Combined);
+  System.out.println("Executed time in nanoseconds: " + Time);
    }
     public void usingBuffer(){
+      long startTime = System.nanoTime();
   StringBuffer Combined=new StringBuffer("");
     for(int i=0;i<names.length;i++){
       Combined.append(names[i]);
     }
+     long endTime = System.nanoTime();
+      long Time = endTime - startTime; 
  System.out.println("By Using StringBuffer: "+Combined);
+  System.out.println("Executed time in nanoseconds: " + Time);
    }
 }
 class concat{
